4fae00f9a414034720deae71f790056a3f50293c
pres220 <58998957+pres220@users.noreply.github.com>
2020-02-06 17:30:17 -0800

    Updates

---
+++ precache-manifest.361ddfcddbf48c1bd1d4dbd2524ca7a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "026523ee0da79677f71f9620abf4ed4e",
+    "url": "/restcountries-navigator/index.html"
+  },
+  {
+    "revision": "b5674116d77ed8d659f8",
+    "url": "/restcountries-navigator/static/css/main.b6c1b530.chunk.css"
+  },
+  {
+    "revision": "668428db2465bbc21b4d",
+    "url": "/restcountries-navigator/static/js/2.2db306d4.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/restcountries-navigator/static/js/2.2db306d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5674116d77ed8d659f8",
+    "url": "/restcountries-navigator/static/js/main.0f164b3d.chunk.js"
+  },
+  {
+    "revision": "915333b391098fd0d6b9",
+    "url": "/restcountries-navigator/static/js/runtime-main.b406c3d3.js"
+  }
+]);
